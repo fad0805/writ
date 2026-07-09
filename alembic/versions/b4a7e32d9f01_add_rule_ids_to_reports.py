3abbb9e98f93958b865302a45a35dca51ad4c8bf
fad0805 <s222y@hotmail.com>
2026-07-09 17:04:15 +0900

    fix: migration 정리 + rule_ids 컬럼 추가

--- alembic/versions/b4a7e32d9f01_add_rule_ids_to_reports.py
+++ alembic/versions/b4a7e32d9f01_add_rule_ids_to_reports.py
@@ -1,25 +1,25 @@
-"""add forward_to_remote to reports
+"""add rule_ids to reports
 
-Revision ID: cb1887b31a75
-Revises: b4c23ee6c9f3
-Create Date: 2026-07-09 16:30:00.000000
+Revision ID: b4a7e32d9f01
+Revises: 1eaa49508f37
+Create Date: 2026-07-09 16:58:00.000000
 
 """
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = 'cb1887b31a75'
-down_revision: Union[str, Sequence[str], None] = None
+revision: str = 'b4a7e32d9f01'
+down_revision: Union[str, Sequence[str], None] = '1eaa49508f37'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     with op.batch_alter_table('reports') as b:
-        b.add_column(sa.Column('forward_to_remote', sa.Boolean(), nullable=True))
+        b.add_column(sa.Column('rule_ids', sa.JSON(), nullable=True))
 
 
 def downgrade() -> None:
     with op.batch_alter_table('reports') as b:
-        b.drop_column('forward_to_remote')
+        b.drop_column('rule_ids')
