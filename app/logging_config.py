2feacee3dfe3bbeb2d46493d2b1b994f8c0e09b0
fad0805 <s222y@hotmail.com>
2026-07-17 11:35:37 +0900

    fix: handle PermissionError in log file creation

--- app/logging_config.py
+++ app/logging_config.py
@@ -8,7 +8,10 @@ _log_file_dir = "logs"
 if _log_file_dir:
     os.makedirs(_log_file_dir, exist_ok=True)
     _log_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    _log_handlers.append(logging.FileHandler(os.path.join(_log_file_dir, f"{_log_date}.log")))
+    try:
+        _log_handlers.append(logging.FileHandler(os.path.join(_log_file_dir, f"{_log_date}.log")))
+    except PermissionError:
+        pass
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",
